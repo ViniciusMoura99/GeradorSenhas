8b1b4ec178a531baac8cc0649403fcb5cad30cbe
VIniciusMoura99 <vinicius.lmoura@hotmail.com>
2025-03-12 19:43:06 -0300

    Finalizado, bug corrigido!

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
 let sliderElement = document.querySelector("#slider"); //atraves do # eu busco o ID
 let buttonElement = document.querySelector("#button");
 
-let sizePassword = document.querySelector("#valor")
-let Password = document.querySelector("#password")
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#password");
 
-let containerPassword = document.querySelector("#container-password")
+let containerPassword = document.querySelector("#container-password");
 
 let charset =  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@$%¨&*(){}[]";
 let novaSenha = "";
 
-sizePassword.innerHTML = sliderElement.Value;
+sizePassword.innerHTML = sliderElement.value;
 
-sliderElement.oninput = function(){
-    sizePassword.innerHTML = this.Value;
+slider.oninput = function(){
+    sizePassword.innerHTML = this.value;
 }
 
 function generatePassword(){
-    alert("VAMOS GERAR A SENHA!")
+    // alert("VAMOS GERAR A SENHA!")
+
+    let pass =""
+    for(let i= 0, n = charset.length; i < sliderElement.value; ++i){
+        pass += charset.charAt(Math.floor(Math.random() * n));
+    }
+
+    containerPassword.classList.remove("hide");
+    password.innerHTML = pass;
+}
+
+function copyPassword(){
+    alert("Senha copiada com sucesso!")
+    navigator.clipboard.writeText(novaSenha);
 }
 
