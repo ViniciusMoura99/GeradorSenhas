62f33216fa7acaf91bcbff4a12a6ee3b03d77d60
VIniciusMoura99 <vinicius.lmoura@hotmail.com>
2025-03-12 19:20:11 -0300

    Script gerado, Erro Undefined no valor de caracteres

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("TESTE")
+let sliderElement = document.querySelector("#slider"); //atraves do # eu busco o ID
+let buttonElement = document.querySelector("#button");
+
+let sizePassword = document.querySelector("#valor")
+let Password = document.querySelector("#password")
+
+let containerPassword = document.querySelector("#container-password")
+
+let charset =  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@$%¨&*(){}[]";
+let novaSenha = "";
+
+sizePassword.innerHTML = sliderElement.Value;
+
+sliderElement.oninput = function(){
+    sizePassword.innerHTML = this.Value;
+}
+
+function generatePassword(){
+    alert("VAMOS GERAR A SENHA!")
+}
+
